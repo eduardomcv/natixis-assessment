9ef9a9fcc78714cb2e1ad0dc215f97f34859a0f6
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-06-08 23:36:45 +0100

    add store to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css'
+import { Store } from "./components/Store";
 
 export function App() {
-  return (
-    <div>App</div>
-  )
+	return (
+		<>
+			<h1>Welcome to the Store</h1>
+			<Store />
+		</>
+	);
 }
-
