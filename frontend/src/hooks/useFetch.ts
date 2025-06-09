66749e3923fa3decf4c83ed054c95f794041b3d0
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-06-09 09:07:40 +0100

    add application/json content type

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -45,9 +45,7 @@ export interface UseFetchOptions {
  * @param options - Options for the fetch request.
  * @returns A tuple containing the current fetch state and a function to trigger the fetch.
  */
-export function useFetch<T>(
-	options: UseFetchOptions,
-): [() => Promise<void>, FetchState<T>] {
+export function useFetch<T>(options: UseFetchOptions) {
 	const { url, method = "GET" } = options;
 
 	const [state, setState] = useState<FetchState<T>>({
@@ -57,33 +55,45 @@ export function useFetch<T>(
 		idle: true,
 	});
 
-	const run = useCallback(async () => {
-		setState({
-			data: undefined,
-			loading: true,
-			error: undefined,
-			idle: false,
-		});
-
-		try {
-			const response = await fetch(url, { method });
-			const data = (await response.json()) as T;
-
-			setState({
-				data,
-				error: undefined,
-				loading: false,
-				idle: false,
-			});
-		} catch (error) {
+	const run = useCallback(
+		async (bodyObject?: Record<string, unknown>) => {
 			setState({
 				data: undefined,
-				loading: false,
-				error: error instanceof Error ? error : new Error("Unknown error"),
+				loading: true,
+				error: undefined,
 				idle: false,
 			});
-		}
-	}, [url, method]);
+
+			try {
+				const body = bodyObject ? JSON.stringify(bodyObject) : undefined;
+
+				const response = await fetch(url, {
+					method,
+					body,
+					headers: {
+						"Content-Type": "application/json",
+					},
+				});
+
+				const data = (await response.json()) as T;
+
+				setState({
+					data,
+					error: undefined,
+					loading: false,
+					idle: false,
+				});
+			} catch (error) {
+				setState({
+					data: undefined,
+					loading: false,
+					error: error instanceof Error ? error : new Error("Unknown error"),
+					idle: false,
+				});
+			}
+		},
+		[url, method],
+	);
 
 	return [run, state] as const;
 }
