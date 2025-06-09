893a08779cadad603a41f3d54611f7074040c12f
Eduardo Vicente <eduardomcvicente@gmail.com>
2025-06-09 02:57:48 +0100

    change headline

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Store } from "./components/Store";
 export function App() {
 	return (
 		<>
-			<h1>Welcome to the Store</h1>
+			<h1>Welcome to the fruits store!</h1>
 			<Store />
 		</>
 	);
